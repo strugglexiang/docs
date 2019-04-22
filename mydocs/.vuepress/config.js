03993141715f9f84b9a249b63c23f9e7439ae133
strugglexiang <576430815@qq.com>
2019-04-22 11:58:01 +0800

    segment introduction

--- mydocs/.vuepress/config.js
+++ mydocs/.vuepress/config.js
@@ -1,3 +1,14 @@
+const {
+    Markdown, 
+    Vue,
+    Redux,
+    designPatter,
+    Babel,
+    webpack,    
+} = require('./introduction')
+
+
+
 module.exports = {
     base: '/dist/',
     port: '8082',
@@ -22,96 +33,16 @@ module.exports = {
                     { text: 'webpack', link:'/webpack/'},
                 ]                
             },
-            // { text: 'Github', link: 'https://github.com/strugglexiang/docs.git' },
+            { text: 'Github', link: 'https://github.com/strugglexiang/docs.git' },
         ],
         sidebarDepth: 3,
         sidebar: {
-            '/markdown/':[
-                {
-                    title: '学习Markdown',
-                    collapsable: false, 
-                    children: [
-                        '',
-                        '教程',
-                    ],
-                },
-            ],
-            '/vue/':[
-                {
-                    title: 'vue指南',
-                    collapsable: false, 
-                    children: [
-                        '',
-                    ],
-                },    
-                {
-                   title: '组件',
-                   children: [
-                       '组件/组件分类',
-                       '组件/组件构成',
-                       '组件/组件内置通信',
-                       '组件/自实现外置通信',
-                       '组件/组件通信终极方案',
-                       '组件/插件通信',
-                       '组件/递归组件',
-                       '组件/动态组件',
-                       '组件/构造器和手动挂载',
-                       '组件/v-model语法糖',
-                       '组件/render与functional'
-                   ] 
-                }            
-            ],
-            '/redux/':[
-                ''
-            ],                                         
-            '/designPattern/':[
-                {
-                    title: '设计模式',
-                    collapsable: false, 
-                    children: [
-                        '',
-                    ],
-                },
-                {
-                    title: '创建型设计模式',
-                    collapsable: false, 
-                    children: [
-                        ['创建型/简单工厂模式', '简单工厂模式']
-                    ]
-                },    
-                {
-                    title: '结构型设计模式',
-                    collapsable: false, 
-                    children: [
-                        ['结构型/外观模式', '外观模式'], 
-                        ['结构型/适配器模式', '适配器模式'],
-                        ['结构型/代理模式', '代理模式'],
-                        ['结构型/组合模式', '组合模式'],
-                    ]
-                },          
-                {
-                    title: '行为型设计模式',
-                    collapsable: false, 
-                    children: [
-                        ['行为型/模板方法模式', '模板方法模式'],
-                        ['行为型/观察者模式', '观察者模式'],
-                        ['行为型/状态模式', '状态模式'],
-                        ['行为型/职责链模式', '职责链模式'],
-                    ]
-                },     
-            ],  
-            '/Babel/': [
-                {
-                    title: 'Babel系列',
-                    collapsable: false, 
-                    children: [
-                        '',
-                    ],
-                }
-            ],
-            '/webpack/': [
-                ''
-            ]              
+            '/markdown/': Markdown,
+            '/vue/': Vue,
+            '/redux/': Redux,                                         
+            '/designPattern/': designPatter,  
+            '/Babel/': Babel,
+            '/webpack/': webpack              
         },
     },
     markdown: {
---
+++ mydocs/.vuepress/introduction.js
@@ -0,0 +1,115 @@
+//------------------------------- Markdown
+const Markdown = [
+    {
+        title: '学习Markdown',
+        collapsable: false, 
+        children: [
+            '',
+            '教程',
+        ],
+    }
+]
+
+
+//------------------------------ Vue
+const Vue = [
+    {
+        title: 'vue指南',
+        collapsable: false, 
+        children: [
+            '',
+        ],
+    },    
+    {
+       title: '组件',
+       children: [
+           '组件/组件分类',
+           '组件/组件构成',
+           '组件/组件内置通信',
+           '组件/自实现外置通信',
+           '组件/组件通信终极方案',
+           '组件/插件通信',
+           '组件/递归组件',
+           '组件/动态组件',
+           '组件/构造器和手动挂载',
+           '组件/v-model语法糖',
+           '组件/render与functional'
+       ] 
+    }       
+]
+
+
+//------------------------------ Redux
+const Redux = [
+    '',
+]
+
+
+//------------------------------- designPatter
+const designPatter = [
+    {
+        title: '设计模式',
+        collapsable: false, 
+        children: [
+            '',
+        ],
+    },
+    {
+        title: '创建型设计模式',
+        collapsable: false, 
+        children: [
+            ['创建型/简单工厂模式', '简单工厂模式']
+        ]
+    },    
+    {
+        title: '结构型设计模式',
+        collapsable: false, 
+        children: [
+            ['结构型/外观模式', '外观模式'], 
+            ['结构型/适配器模式', '适配器模式'],
+            ['结构型/代理模式', '代理模式'],
+            ['结构型/组合模式', '组合模式'],
+        ]
+    },          
+    {
+        title: '行为型设计模式',
+        collapsable: false, 
+        children: [
+            ['行为型/模板方法模式', '模板方法模式'],
+            ['行为型/观察者模式', '观察者模式'],
+            ['行为型/状态模式', '状态模式'],
+            ['行为型/职责链模式', '职责链模式'],
+        ]
+    },     
+]
+
+
+//------------------------------- Babel
+const Babel = [
+    {
+        title: 'Babel系列',
+        collapsable: false, 
+        children: [
+            '',
+        ],
+    }    
+]
+
+
+
+//------------------------------- webpack
+const webpack =  [
+    ''
+]
+
+
+
+//--------------------------------- export
+module.exports= {
+    Markdown, 
+    Vue,
+    Redux,
+    designPatter,
+    Babel,
+    webpack,
+}
